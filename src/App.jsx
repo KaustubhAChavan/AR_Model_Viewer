da27c415ff91e7827a4e87aacdae9cf2975f7c39
Kaustubh Ashok Chavan <kaustubhashokchavan@Kaustubhs-MacBook-Pro.local>
2025-12-15 20:10:01 +0530

    backend testing port

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
       formData.append('file', file);
 
       // Call your FastAPI endpoint
-      const response = await fetch('http://127.0.0.1:8080/convert', {
+      const response = await fetch('http://217.171.200.22:51754/convert', {
         method: 'POST',
         body: formData,
       });
