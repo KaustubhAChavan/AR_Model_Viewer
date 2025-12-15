35c7e1303d8830691fb9cb330fc76d2d79ba4e45
Kaustubh Ashok Chavan <kaustubhashokchavan@Kaustubhs-MacBook-Pro.local>
2025-12-15 18:44:48 +0530

    backend testing fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 export default defineConfig({
   plugins: [react(), basicSsl()],
   server: {
-    host: true, // Exposes IP to network
-    https: true // Enables HTTPS
+    host: true
   }
 })
